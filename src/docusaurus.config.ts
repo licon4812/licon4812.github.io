0764fb01a873ff0efc5c7677525f4ff01396394c
James Alickolli <jalickolli@gmail.com>
2025-07-02 17:23:40 +1000

    Update docusaurus.config.ts

--- src/docusaurus.config.ts
+++ src/docusaurus.config.ts
@@ -23,8 +23,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'licon4812', // Usually your GitHub org/user name.
+  projectName: 'licon4812.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
